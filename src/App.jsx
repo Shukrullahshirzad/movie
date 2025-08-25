f3294dfeced8f2b61dbdac47fc7c836b3194a810
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-08-25 11:36:27 +0430

    add project structure and asset files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <main>
+            <div className="pattern">
+                <div className="wrapper">
+                    <header>
+                        <img src="./hero.png" alt="" />
+                        <h1>Find <span className="text-gradient">Movies</span> You'll Like Without the Hassel</h1>
+                    </header>
+                </div>
+            </div>
+        </main>
+    )
+}
