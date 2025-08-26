238b59f34ac0bd9d00b1cc042006e9c7f00ec285
shukrullahshirzad <shukrullah.shirzad.779@gmail.com>
2025-08-26 15:12:33 +0430

    add hover effect to card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import star from "../assets/star.svg"
-
+import star from "../assets/star.svg";
 
 function MovieCard({
   movie: { title, vote_average, poster_path, release_date, original_language },
 }) {
   return (
-    <div className="movie-card">
+    <div className="movie-card hover:bg-gray-800 group hover:cursor-pointer">
       <img
+        className="transition-transform duration-300 group-hover:scale-[1.03]"
         src={
           poster_path
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
             : "/no-movie.png"
         }
         alt={title}
-          />
-          <h3>{title}</h3>
-          <div className="content">
-              <div className="rating">
-                  <img src={star} alt="star icon" />
-                  <p>{vote_average ? vote_average.toFixed(1):"N/A"} </p>
-              </div>
-              <span>•</span>
-              <p className="lang">{original_language}</p>
-              <span>•</span>
-              <p className="year">{release_date? release_date.split('-')[0]: "N/A"}</p>
-          </div>
+      />
+      <h3>{title}</h3>
+      <div className="content ">
+        <div className="rating ">
+          <img className="" src={star} alt="star icon" />
+          <p>{vote_average ? vote_average.toFixed(1) : "N/A"} </p>
+        </div>
+        <span>•</span>
+        <p className="lang">{original_language}</p>
+        <span>•</span>
+        <p className="year">
+          {release_date ? release_date.split("-")[0] : "N/A"}
+        </p>
+      </div>
     </div>
   );
 }
